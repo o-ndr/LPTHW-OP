636ba89d5081c65a0a4a34e2ef45165b4f5e0562
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2016-02-22 21:40:11 -0500

    Added comments to ex24.py

--- ex24.py
+++ ex24.py
@@ -42,4 +42,7 @@ start_point = start_point / 10
 
 print "We can also do that this way:"
 print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+# This version calls on the function and fills in the values
+# directly, without creating intermediate variables outside the function
+
 
