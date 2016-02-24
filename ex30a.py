5fbc206691bc1f913b36023206b2e0635bc4676b
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2016-02-24 15:21:20 -0500

    Ex 30a.py with more examples of if statement

---
+++ ex30a.py
@@ -0,0 +1,39 @@
+# Ex 30, added stuff
+# Source: http://codepad.org/VFexAiYM 
+
+# this is how else works
+
+if 5 > 10:
+    print "Five is more than ten."
+elif 20 < 10:
+    print "Twenty is less than ten."
+elif 17 == 10:
+    print "Seventeen is equal to ten"
+elif 1 > 500:
+    print "One is more than five hundred."
+else:
+    print "None of those were true."
+
+if "black" == "red":
+    print "Black and red are the same."
+elif 2 < 3:
+    print "Two is less than three."
+elif 3 < 4:
+    print "Three is less than four."
+elif 4 < 5:
+    print "Four is less than five."
+else:
+    print "None of those were true."
+	
+# This shows how the if statement works if the very first line
+# evaluates as True
+if 0 == 0:
+    print "Zero and zero are the same."
+elif 2 < 3:
+    print "Two is less than three."
+elif 3 < 4:
+    print "Three is less than four."
+elif 4 < 5:
+    print"Four is less than five."
+else:
+    print "None of those were true."
