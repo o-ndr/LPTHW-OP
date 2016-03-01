45e5c4f531fd2d27f984b9c9cc97a838d7d1a5c9
Oleksandr Pysaryuk <alexpysaryuk@gmail.com>
2016-02-29 21:31:50 -0500

    Ex34 edited and new ex35 with a game added.

--- ex34.py
+++ ex34.py
@@ -1,1 +1,7 @@
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+print animals
+for i in animals:
+	print i
+
+for i in range(len(animals)):
+	print "Index %d in the list is %s." % (i, animals[i])
